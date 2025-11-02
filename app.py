04396fd686f9380b5b34b1a3a2d80f1a9c2e8ded
salem <seme774993794@gmail.com>
2025-11-02 10:19:32 +0100

    Increase password_hash field length to 255

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def add_header(response):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
